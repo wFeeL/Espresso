8e8e601a6e06a0517dec1f1bb87d310440ed75aa
Student <danny.glazovs@gmail.com>
2023-11-23 18:25:05 +0300

    Add some function for programm

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import sqlite3
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QFileDialog, QPushButton, QColorDialog
 
-con = sqlite3.connect('coffee.sqlite')
-cur = con.cursor()
 
-print(cur.execute('SELECT * FROM coffee').fetchall())
+class Coffee(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(1100, 400, 800, 500)
+        self.setWindowTitle('Coffee')
+
+        self.con = sqlite3.connect('coffee.sqlite')
+        self.cur = self.con.cursor()
+
+        self.button.clicked.connect(self.show_result)
+
+    def show_result(self):
+        result = self.cur.execute('SELECT * FROM coffee '
+                                  'ORDER BY id')
+        return result
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Coffee()
+    ex.show()
+    sys.exit(app.exec())
